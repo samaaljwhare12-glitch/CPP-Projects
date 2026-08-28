6e500d01b82ca06689e49ee27c943d8e37957770
Sama <samaaljwhare12@gmail.com>
2026-08-28 22:29:31 +0300

    Create number_pattern.cpp

---
+++ number_pattern.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 9; i++) {
+        if (i <= 5) {
+            cout << i << " ";
+        } else {
+            cout << (10 - i) << " ";
+        }
+    }
+
+    cout << endl;
+
+    return 0;
+}
